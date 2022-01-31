63c24182d2aa8eea527b0c3ac012d4de5a50b1da
Andy Perrett <18684092@students.lincoln.ac.uk>
2022-01-31 13:41:01 +0000

    Testing wifi and gsm

---
+++ modules/WiFi.py
@@ -0,0 +1,24 @@
+import network
+import urequests 
+import time
+
+SSID = 'ASK4 Wireless'
+PASS = ''
+TEST_SITE = 'https://jsonplaceholder.typicode.com/todos/1' 
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(SSID, PASS)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())   
+    testConnectivity()
+
+
+def testConnectivity():
+    r = urequests.get(url=TEST_SITE)
+    print(r.json()['id'])
+
